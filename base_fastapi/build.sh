c4aee554922ba617a8a91b58d310b25337740744
Andrei Damian <andrei.damian@me.com>
2024-02-10 21:16:54 +0200

    first build for fastapi

---
+++ base_fastapi/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Default Python version
+PYTHON_VERSION=${1:-3.11}-slim
+
+IMAGE_NAME="aidamian/base_fastapi"
+
+# Replace dots with underscores and remove minor version if it's zero, for tagging purposes
+IMAGE_TAG="py${PYTHON_VERSION}"
+FULL_IMAGE_NAME="$IMAGE_NAME:$IMAGE_TAG"
+
+# Build the Docker image with a dynamic tag based on the Python version
+echo "Building Docker image with Python version $PYTHON_VERSION"
+docker build --build-arg PYTHON_VERSION=${PYTHON_VERSION} -t $FULL_IMAGE_NAME .
+
+# # Push the Docker image with the dynamic tag
+echo "Pushing Docker image $FULL_IMAGE_NAME"
+docker push $FULL_IMAGE_NAME
+
+

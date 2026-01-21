4129f62dc9e8fdf76ece438d2cc7b9fe23af5e68
ispritraian <traian.ispir@yahoo.com>
2026-01-21 23:31:14 +0200

    fix: xpermiental dcoekr images using python 3.13, pythorch 2.9.0, tensorRT 10.14.1

--- xperimental/scripts/build-ffmpeg.sh
+++ xperimental/scripts/build-ffmpeg.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
+set -euo pipefail
 
-# Bail out on errors.
-set -e
+FFMPEG_VERSION="${FFMPEG_VERSION:-6.1.1}"
+DAV1D_VERSION="${DAV1D_VERSION:-1.4.1}"
+OPENH264_VERSION="${OPENH264_VERSION:-2.2.0}"
+SVT_AV1_VERSION="${SVT_AV1_VERSION:-2.0.0}"
+PREFIX="/usr/local"
 
-# This builds ffmpeg 6.1 from source. We're matching the same configuration
-# of ffmpeg as that that we would get from conda. Note there are pottentially
-# more codecs that we could build (including proprietary ones), but these are
-# clearly not used at this point.
-
-# apt-get install all codecs that we can actually install on 20.04.
-apt-get install -y --no-install-recommends \
-  libva-dev \
-  vainfo \
-  libgnutls28-dev \
-  libaom-dev \
-  libmp3lame-dev \
-  libopus-dev \
-  libvpx-dev \
-  libx264-dev \
-  libx265-dev \
-  libxml2-dev \
-  libass-dev
+cd /tmp
 
 # Build the dav1d codec.
-git clone https://code.videolan.org/videolan/dav1d.git dav1d
+git clone --depth 1 --branch "${DAV1D_VERSION}" https://code.videolan.org/videolan/dav1d.git dav1d
 cd dav1d
-git checkout 1.4.1
 mkdir -p build
 cd build
 meson setup \
@@ -33,41 +19,39 @@ meson setup \
   -Denable_tests=false \
   --default-library=static \
   .. \
-  --prefix "/usr/lib" \
-  --libdir="/usr/lib"
+  --prefix "${PREFIX}" \
+  --libdir="${PREFIX}/lib"
 ninja
 ninja install
 cd /tmp
 rm -rf dav1d
 
 # Build the openh264 codec.
-git clone https://github.com/cisco/openh264.git openh264
+git clone --depth 1 --branch "openh264v${OPENH264_VERSION}" https://github.com/cisco/openh264.git openh264
 cd openh264
-git checkout openh264v2.2.0
-make -j8
-make install
+make -j"$(nproc)"
+make install PREFIX="${PREFIX}"
 cd /tmp
 rm -rf openh264
 
-# Build the SVT-AV1 codec
-git clone https://gitlab.com/AOMediaCodec/SVT-AV1/
-cd SVT-AV1/
-git checkout v2.0.0
-mkdir build
-cd build/
-cmake  -GNinja -DCMAKE_BUILD_TYPE=Release ../
+# Build the SVT-AV1 codec.
+git clone --depth 1 --branch "v${SVT_AV1_VERSION}" https://gitlab.com/AOMediaCodec/SVT-AV1.git svt-av1
+cd svt-av1
+mkdir -p build
+cd build
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${PREFIX}" ../
+ninja
 ninja install
 cd /tmp
-rm -rf SVT-AV1
+rm -rf svt-av1
 
-# Now build ffmpeg. Use the same configuration options as
-# used in the conda-forge build.
-git clone https://github.com/FFmpeg/FFmpeg.git ffmpeg
-cd ffmpeg
-
-# ffmpeg version n6.1.1-34-g5e45c27ba9
-git checkout 5e45c27ba9baf576e548e226162be7e104328cc0
+# Build ffmpeg from a pinned release.
+cd /tmp
+curl -fsSLO "https://ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.xz"
+tar -xf "ffmpeg-${FFMPEG_VERSION}.tar.xz"
+cd "ffmpeg-${FFMPEG_VERSION}"
 ./configure \
+  --prefix="${PREFIX}" \
   --disable-doc \
   --disable-openssl \
   --enable-demuxer=dash \
@@ -94,7 +78,7 @@ git checkout 5e45c27ba9baf576e548e226162be7e104328cc0
   --enable-version3 \
   --enable-zlib \
   --enable-libopus
-make -j16
+make -j"$(nproc)"
 make install
 cd /tmp
-rm -rf ffmpeg
+rm -rf "ffmpeg-${FFMPEG_VERSION}" "ffmpeg-${FFMPEG_VERSION}.tar.xz"
